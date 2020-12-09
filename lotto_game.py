8d1d93f0adba5a6b7b5595551969dd26fea4cf2c
Roberta <RobertaRuggiero_97@hotmail.it>
2020-12-09 23:15:22 +0100

    updated lotto_game

--- lotto_game.py
+++ lotto_game.py
@@ -1,61 +1,78 @@
 # Import methods from other files
-from lotto.bills import Lotto
+from lotto.lotto import Lotto
 from argparse import ArgumentParser
-from lotto.prints import PrintTickets
+from lotto.print import Print
+from lotto.extraction import Extraction
 
 def main():
-  arg_par = ArgumentParser(description="Lotto ticket generator")
-  arg_par.add_argument("n", type = int, help="Number of ticket to generate", choices = [0, 1, 2, 3, 4, 5])
-  args = arg_par.parse_args()
+    arg_par = ArgumentParser(description="Lotto ticket generator")
+    arg_par.add_argument("n", type = int, help="Number of ticket to generate", choices = [0, 1, 2, 3, 4, 5])
+    args = arg_par.parse_args()
   # Associates the entered number by cmn line to tickets_num
-  tickets_num = args.n
+    tickets_num = args.n
 
-  while True:
-   try:
+    if tickets_num is None:
+        Print.horizontal_line()
+        Print.central("You can choice from 1 to 5 numbers of tickets")
+        Print.central("or 0 to quit")
+        Print.horizontal_2line()
+        tickets_numb = int(input(" - How many tickets do you want to generate?: "))
+
+    while True:
+        try:
     # Case where zero entered --> exit
-    if tickets_num == 0:
-        PrintTickets.horizontal_line()
-        print("       !!ATTENTION ENTERED 0!!\n      Thanks for participating,\n          see you next time!")
-        PrintTickets.horizontal_under()
-        print()
-        print("Quitting...")
-        break
+            if tickets_num == 0:
+                Print.horizontal_line()
+                Print.central("!!ATTENTION ENTERED 0!!")
+                Print.central("Thanks for participating,")          
+                Print.central("see you next time!")
+                Print.horizontal_2line()
+                print()
+                print("Quitting...")
+                break
     # If the number entered is in the desired range
-    elif tickets_num>=1 and tickets_num<=5:
-        ticket = Lotto()
-        ticket.ticket_amount = tickets_num
-        ticket.print_ticket(ticket,tickets_num)
+            elif tickets_num>=1 and tickets_num<=5:
+                lotto = Lotto()
+                lotto.ticket_amount = tickets_num
+                choices = lotto.generate_tickets(tickets_num)
 
-        print()
-        PrintTickets.horizontal_line()
-        print("            !!ATTENTION!!\n       Do you want to continue?")
-        PrintTickets.horizontal_under()
-        print()
-        tickets_num = int(input("- Yes ---> Enter a number between 1 and 5: \n" \
-                                "-  No ---> Enter 0 to quit: \n"\
-                                "\nEnter here: "))
+                extraction = Extraction()
+                extraction.print_extraction()
+
+                lotto.lotto_tickets(lotto,choices,extraction)
+                print()
+                Print.horizontal_line()
+                Print.central("!!ATTENTION!!")
+                Print.central("Do you want to continue?")
+                Print.horizontal_2line()
+                print()
+                tickets_num = int(input("- Yes ---> Enter a number of ticket (from 1 to 5): \n" \
+                                        "-  No ---> Enter 0 to quit: \n"\
+                                        "\nEnter here: "))
     # # If the number entered is out of range
-    else:
-        PrintTickets.horizontal_line()
-        print("            !!ATTENTION!!\n     That number is out of range!")
-        PrintTickets.horizontal_under()
-        print()
-        print("Please, try again...")
-        print()
-        tickets_num = int(input("- Yes ---> Enter a number between 1 and 5 \n" \
-                   "-  No ---> Enter 0 to quit: \n"\
-                   "Enter here: "))
-   # If a non-numeric value is entered                
-   except ValueError: 
-       PrintTickets.horizontal_line()
-       print("            !!ATTENTION!!\n           Enter a number!")
-       PrintTickets.horizontal_under()
-       print()
-       print("Please, try again...")
-       print()
-       tickets_num = int(input("- Yes ---> Enter a number between 1 and 5 \n" \
-                   "-  No ---> Enter 0 to quit: \n"\
-                   "Enter here: "))
+            else:
+                Print.horizontal_line()
+                Print.central("!!ATTENTION!!")
+                Print.central("That number is out of range!")
+                Print.horizontal_2line()
+                print()
+                print("Please, try again...")
+                print()
+                tickets_num = int(input("- Yes ---> Enter a number of ticket (from 1 to 5): \n" \
+                                        "-  No ---> Enter 0 to quit: \n"\
+                                        "\nEnter here: "))
+      # If a non-numeric value is entered                
+        except ValueError: 
+            Print.horizontal_line()
+            Print.central("!!ATTENTION!!")
+            Print.central("Enter a number!")
+            Print.horizontal_2line()
+            print()
+            print("Please, try again...")
+            print()
+            tickets_num = int(input("- Yes ---> Enter of ticket (from 1 to 5): \n" \
+                                    "-  No ---> Enter 0 to quit: \n"\
+                                    "Enter here: "))
 
     
 
